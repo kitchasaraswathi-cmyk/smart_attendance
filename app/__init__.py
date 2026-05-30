42fb06e476863b2a06dc2287b7efa53b9ab26a37
Saraswathi S <kitchasaraswathi@gmail.com>
2026-05-30 10:29:13 +0530

    Implemented Flask-Mail email notifications

--- app/__init__.py
+++ app/__init__.py
@@ -29,6 +29,7 @@ def create_app():
     app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS') == 'True'
     app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
     app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+    app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
     
 
     @app.route('/')
